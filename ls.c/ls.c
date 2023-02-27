6238aa38452ba3533118a7eab23eb6f503a7c40d
duyujiao <2637599327@qq.com>
2023-02-27 16:26:44 +0800

    dirent

--- ls.c/ls.c
+++ ls.c/ls.c
@@ -1,13 +1,10 @@
- //DIR结构体定义，关于目录信息
- struct __dirstream
-    {
-     void *__fd;
-     char *__data;
-     int __entry_data;
-     char *__ptr;
-     int __entry_ptr;
-     size_t __allocation;
-     size_t __size;
-     __libc_lock_define (, __lock)
-    };
- typedef struct __dirstream DIR;
+//dirent结构体，指向目录和目录中的具体函数
+struct dirent
+{
+     long d_ino;     	/* 索引节点号 */
+     off_t d_off; 		/*在目录文件中的偏移 */
+     unsigned short d_reclen; 		/*文件名长 */
+     unsigned char d_type; 			/*文件类型 */
+     char d_name [NAME_MAX+1];	 	/* 文件名，最长255字符 */
+ }
+
